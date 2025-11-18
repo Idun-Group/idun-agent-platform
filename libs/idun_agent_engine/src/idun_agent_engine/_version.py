a35cc05a2b74557f99418f5f293be5a018460e98
Geoffrey HARRAZI <geoffreyharrazi@gmail.com>
2025-11-18 19:39:57 +0100

    improve docker for web

--- libs/idun_agent_engine/src/idun_agent_engine/_version.py
+++ libs/idun_agent_engine/src/idun_agent_engine/_version.py
@@ -1,7 +1,3 @@
 """Version information for Idun Agent Engine."""
 
-<<<<<<< HEAD
-__version__ = "0.2.5"
-=======
-__version__ = "0.2.4"
->>>>>>> cd714c2 (Develop (#89))
+__version__ = "0.2.6"
--- libs/idun_agent_schema/src/idun_agent_schema/__init__.py
+++ libs/idun_agent_schema/src/idun_agent_schema/__init__.py
@@ -6,8 +6,4 @@ Public namespaces:
 - idun_agent_schema.shared: Shared cross-cutting schemas
 """
 
-<<<<<<< HEAD
-__version__ = "0.2.5"
-=======
-__version__ = "0.2.4"
->>>>>>> cd714c2 (Develop (#89))
+__version__ = "0.2.6"
