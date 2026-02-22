c9534204ad8f8666ad01382bd486f9f6c5da7264
wiz-of-idun <e.yassin@idun-group.com>
2026-02-22 15:50:56 +0100

    Rebase

--- services/idun_agent_web/src/App.tsx
+++ services/idun_agent_web/src/App.tsx
@@ -39,6 +39,7 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signin" element={<SigninPage />} />
 
+
                 {/* Protected routes */}
                 <Route element={<RequireAuth />}>
                     <Route
