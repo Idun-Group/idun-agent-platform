1bbeb16f3335bb3791709e12bb606423bc0901bb
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2026-02-27 14:28:12 +0100

    fix(manager-init): fix merge issues

--- libs/idun_agent_schema/src/idun_agent_schema/manager/__init__.py
+++ libs/idun_agent_schema/src/idun_agent_schema/manager/__init__.py
@@ -7,13 +7,14 @@ from .managed_agent import (  # noqa: F401
     ManagedAgentPatch,
     ManagedAgentRead,
 )
-from .managed_mcp_server import (  # noqa: F401
-    MCPToolSchema,
-    MCPToolsResponse,
 from .managed_integration import (  # noqa: F401
     ManagedIntegrationCreate,
     ManagedIntegrationPatch,
     ManagedIntegrationRead,
+)
+from .managed_mcp_server import (  # noqa: F401
+    MCPToolSchema,
+    MCPToolsResponse,
 )
 from .managed_sso import (  # noqa: F401
     ManagedSSOCreate,
